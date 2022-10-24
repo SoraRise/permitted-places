199ad8a9c300f6188aff7babdbed113abfe47605
Daniil Rise <ashuramaru@Daniils-MacBook-Pro.local>
2022-10-24 22:20:19 +0300

    divided into packages

--- internal/domain/entity/places.go
+++ internal/domain/entity/places.go
@@ -1,26 +1,11 @@
 package entity
-import(
-	"context"
-)
+
 type Places struct {
 	Id int `json:"id"`
 	Latitude string `json:"latitude" validate:"required`
 	Longitude string `json:"longitude" validate:"required"`
 }
 
-type PlacesUsecase interface {
-	GetById(ctx context.Context, id int) (Places, error)
-	UpdatePlaces(ctx context.Context, pl *Places) error
-	DeletePlaces(ctx context.Context, id Places) error
-	SaveNewPlaces(ctx context.Context, pl Places) error
-}
-
-type PlacesRepository interface {
-	GetById(ctx context.Context, id int) (model Places, err error)
-	UpdatePlaces(ctx context.Context, pl *Places) (err error)
-	DeletePlaces(ctx context.Context, id Places) (err error)
-	SaveNewPlaces(ctx context.Context, pl Places) (err error)
-}
 
 
 
